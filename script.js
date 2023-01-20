b2639059534e7cf05f76d4dd4a2bc28fe8c57cda
kanish chauhan <kanishchauhan959@gmail.com>
2023-01-20 22:35:58 +0530

    button highliting done

--- script.js
+++ script.js
@@ -10,8 +10,73 @@ let textarea=document.getElementById('text-input')
 
 let linkbutton=document.getElementById('link')
 
-let alignbuttons=document.querySelectorAll('.align')
-let spacing=document.querySelectorAll('/spacing')
-let format=document.querySelectorAll('.format')
-let script=document.querySelectorAll('.script')
+let alignbuttons=document.querySelectorAll('.algn')
+let spacingbuttons=document.querySelectorAll('.spacing')
+let formatbuttons=document.querySelectorAll('.format')
+let scriptbuttons=document.querySelectorAll('.script')
 
+let fontlist=[
+    'Arial','Verdana','Times New Roman','Garamond','Georgia','Courier New','Cursive'
+]
+const initializer=()=>{
+    highlighter(alignbuttons,true)
+    highlighter(spacingbuttons,true)
+    highlighter(formatbuttons,false)
+    highlighter(scriptbuttons,true)
+    
+
+    fontlist.map((value)=>{
+        let option =document.createElement('option')
+        option.value=value
+        option.innerHTML=value;
+        option.style.textAlign='left'
+        fontname.appendChild(option)
+
+
+    })
+
+    for(let i=4;i<=14;i=i+2){
+        let option=document.createElement('option')
+        option.value=i
+        option.innerHTML=i
+        fontsize.appendChild(option)
+
+    }
+}
+
+const highlighter=(className,state)=>{
+    className.forEach((e)=>{
+    e.addEventListener('click',()=>{
+
+        if(state){
+            let alreadyactive=false
+            
+            if(e.classList.contains('active')){
+                alreadyactive=true
+            }
+            
+            removehighlighter(className);
+            if(!alreadyactive){
+                e.classList.add('active')
+            }
+        }
+        else{
+                e.classList.toggle('active')
+            }
+            
+    })  
+        
+    
+    });
+}
+
+
+
+// window.onload=initializer()
+const removehighlighter=(className)=>{
+    className.forEach((e)=>{
+        e.classList.remove('active')
+    })
+    
+}
+initializer()
