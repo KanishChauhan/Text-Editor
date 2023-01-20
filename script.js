fc64032b527696170ff9ef07c4ee5793abae1c8a
kanish chauhan <kanishchauhan959@gmail.com>
2023-01-20 20:15:49 +0530

    buttons added

---
+++ script.js
@@ -0,0 +1,17 @@
+let options=document.querySelectorAll('.nor')
+let adv=document.querySelectorAll('.adv')
+// let adv=document.createElement('adv')
+
+let fontname=document.getElementById('fontname')
+
+let fontsize=document.getElementById('fontsize')
+
+let textarea=document.getElementById('text-input')
+
+let linkbutton=document.getElementById('link')
+
+let alignbuttons=document.querySelectorAll('.align')
+let spacing=document.querySelectorAll('/spacing')
+let format=document.querySelectorAll('.format')
+let script=document.querySelectorAll('.script')
+
